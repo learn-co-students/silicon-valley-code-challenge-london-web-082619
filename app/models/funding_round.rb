2635bb82be8cef0ef55c072a93511b8e3453525f
Angus Morrison <contact@angus-morrison.com>
2019-08-30 14:13:30 +0100

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,28 @@
 class FundingRound
+    attr_reader :startup, :venture_capitalist, :type
 
+    @@all = []
+
+    #Working!
+    def initialize(startup, vc, type, investment)
+        @startup = startup
+        @venture_capitalist = vc
+        @type = type
+        @investment = investment
+        @@all << self
+    end
+
+    #Working!
+    def self.all
+        @@all
+    end
+
+    #Working
+    def investment
+        if @investment > 0
+            return @investment.to_f().round(2)
+        else
+            return 0
+        end
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,34 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+founders = ["Angus Morrison", "Bianca Bass", "Trudy Morrison", "Chris Morrison"]
+startups = ["Uber", "Facebook", "ClearScore", "Stripe", "PayPal"]
+domains = ["finance", "ecommerce", "travel", "retail", "gaming"]
 
-binding.pry
+vcs = ["Big Corp", "MegaFund", "SoftBank", "Banco Massivo"]
+worths = [1_200_000_000, 1000, 1_000_000, 1_000_000_000_000]
+
+types = ["Angel", "Pre-Seed", "Seed", "Series A", "Series B", "Series C"]
+investments = [100.00, 3000000, 500000000, 7, 501235234.30]
+
+10.times { Startup.new(startups.sample, founders.sample, domains.sample) }
+10.times { VentureCapitalist.new(vcs.sample, worths.sample) }
+50.times { FundingRound.new(Startup.all.sample, VentureCapitalist.all.sample, types.sample, investments.sample)}
+
+###### Startups ######
+
+test_startup = Startup.all.sample
+p test_startup.name
+p test_startup.funding_rounds
+
+
+=begin
+###### VCs ######
+test_vc = VentureCapitalist.all.sample
+p test_vc
+p test_vc.invested("gaming")
+=end
+
+
+
 0 #leave this here to ensure binding.pry isn't the last line
