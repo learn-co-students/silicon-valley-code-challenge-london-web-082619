64aed4b62723fa5b0c27ba0856ce693a151530c2
Matthew Dias Prada <matgems@googlemail.com>
2019-08-30 18:26:25 +0100

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,44 @@
-class VentureCapitalist
 
+class VentureCapitalist
+    attr_reader :name , :total_worth
+
+    @@all = []
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.tres_commas_club
+        self.all.select {|v| v.total_worth > 1000000000 }
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(self, startup, type, investment)
+    end
+
+    def startups
+        funding_rounds.map {|round| round.startup}
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|round| round.venture_capitalist == self}
+    end
+
+    def portfolio
+        startups.uniq
+    end
+
+    def biggest_investment
+        portfolio.max_by {|startup| startup.investment}
+    end
+
+    def invested(domain)
+        startups2 = startups.select {|startup| startup.domain == domain}
+        startups2.sum {|startup| startup.total_funds}
+    end
+
+    def self.all
+        @@all
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,49 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+founders = ["Bob", "Frank", "Paul", "John", "George", "Ringo"]
+startups = ["meganet", "flatiron", "velonet", "twr", "google", "yahoo"]
+domains = ["mega.net", "flatiron.school", "velo.net", "google.com", "yahoo.who?"]
+vc = ["billy", "bobby", "will", "robert", "william", "rob"]
+funding = ["Angel", "Pre-Seed", "Seed", "Series A", "Series B", "Series C"]
+
+def total_worth
+  rand(10000000..10000000000).ceil(-4)
+end
+
+def investment
+  rand(1000..1000000).ceil(-3)
+end
+
+startup1 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup2 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup3 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup4 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup5 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup6 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup7 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup8 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup9 = Startup.new(startups.sample, founders.sample, domains.sample )
+startup10 = Startup.new(startups.sample, founders.sample, domains.sample )
+
+vencap1 = VentureCapitalist.new(vc.sample, total_worth)
+vencap2 = VentureCapitalist.new(vc.sample, total_worth)
+vencap3 = VentureCapitalist.new(vc.sample, total_worth)
+vencap4 = VentureCapitalist.new(vc.sample, total_worth)
+vencap5 = VentureCapitalist.new(vc.sample, total_worth)
+vencap6 = VentureCapitalist.new(vc.sample, total_worth)
+vencap7 = VentureCapitalist.new(vc.sample, total_worth)
+vencap8 = VentureCapitalist.new(vc.sample, total_worth)
+vencap9 = VentureCapitalist.new(vc.sample, total_worth)
+vencap10 = VentureCapitalist.new(vc.sample, total_worth)
+
+fund1 = startup1.sign_contract(vencap1, funding.sample, investment)
+fund2 = startup2.sign_contract(vencap2, funding.sample, investment)
+fund3 = startup3.sign_contract(vencap3, funding.sample, investment)
+fund4 = startup4.sign_contract(vencap4, funding.sample, investment)
+fund5 = startup5.sign_contract(vencap5, funding.sample, investment)
+fund6 = startup6.sign_contract(vencap6, funding.sample, investment)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
